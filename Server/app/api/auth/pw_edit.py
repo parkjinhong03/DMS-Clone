feb0d2a3a08579f1d03005e78ce863b3e26f448a
parkjinhong03 <jinhong0719@naver.com>
2019-08-05 14:33:08 +0900

    status code 로그인 상태 X -> 410 통일

--- Server/app/api/auth/pw_edit.py
+++ Server/app/api/auth/pw_edit.py
@@ -10,8 +10,8 @@ def pw_edit():
     :parameter: pw, pw_check
     :return: status code
     200 - 비밀번호 변경 완료
-    410 - 비밀번호와 비밀번호 확인 값이 다름
-    411 - 로그인 상태 아님
+    410 - 로그인 상태 아님
+    411 - 비밀번호와 비밀번호 확인 값이 다름
     '''
 
     data = request.form
@@ -20,10 +20,10 @@ def pw_edit():
     pw_check = data['pw_check']
 
     if 'user' not in request.cookies:
-        return 'login first!', 411
+        return 'login first!', 410
 
     if pw != pw_check:
-        return 'pw and pw_check are different', 410
+        return 'pw and pw_check are different', 411
 
     user_id = request.cookies.get('user')
 
