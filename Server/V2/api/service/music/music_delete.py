4e020d46e424978460270c9e00d31d15317deb27
parkjinhong03 <jinhong0719@naver.com>
2019-08-23 09:38:24 +0900

    음악 취소 (DB DELETE) 기능 추가

--- Server/V2/api/service/music/music_delete.py
+++ Server/V2/api/service/music/music_delete.py
@@ -2,29 +2,35 @@
 기상 음악 취소 모듈
 '''
 
+from Server.V2.DB_func.connect import connect
+from Server.V2.DB_func.service.exist_music import exist_music
+from Server.V2.api.cookie_decorator import login_required
 from flask import Flask, request, make_response
 import os
 
+
+@login_required
 def music_delete():
     '''
     :parameter: X
     :return: status code
     403 로그인 상태 아님
+    410 전에 신청해둔 음악이 없음.
     200 기상 음악 신청 취소 완료
     '''
 
-    if 'user' not in request.cookies:
-        return '로그인을 먼저 해주세요.', 403
     my_name = request.cookies.get('user')
 
-    date_list = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri']
+    con, cur = connect()
 
-    count = 0
-    for i in date_list:
-        if os.path.exists('V1/data/Music/'+i+'/'+my_name):
-            os.remove('V1/data/Music/'+i+'/'+my_name)
-            count += 1
-            return '기상 음악 취소 완료!', 200
+    if exist_music(con, cur, my_name) == False:
+        return "전에 신청해둔 음악이 없습니다.", 410
 
-    if count == 0:
-        return '전에 신청해준 기상 음악이 없습니다.', 200
+    sql = f'DELETE FROM music WHERE user_id="{my_name}"'
+
+    cur.execute(sql)
+
+    con.commit()
+    con.close()
+
+    return "기상 음악 취소 완료!", 200
